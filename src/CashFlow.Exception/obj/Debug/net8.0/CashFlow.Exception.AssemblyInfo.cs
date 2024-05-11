cd529bce934f537272a1c3c14dc97c9c15520b45
caiobretas <caiodinizbretas2001@gmail.com>
2024-05-11 19:59:47 -0300

    Add CommonTestUtilities.csproj

--- src/CashFlow.Exception/obj/Debug/net8.0/CashFlow.Exception.AssemblyInfo.cs
+++ src/CashFlow.Exception/obj/Debug/net8.0/CashFlow.Exception.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Exception")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e03d40b0277c039b7ce18877a8026a3808e98a6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60f9299e13c435bc003b5d776d68a07a466c42f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Exception")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Exception")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
