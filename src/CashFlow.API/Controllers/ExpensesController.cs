3cab55a55f6c65eb0b594da9e7ab2c9d06c9bcf4
caiobretas <caiodinizbretas2001@gmail.com>
2024-05-27 10:37:19 -0300

    Add GetAll endpoint

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -1,5 +1,9 @@
+using CashFlow.Application.UseCases.Expenses.GetAll;
 using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communications.Requests.Register;
+using CashFlow.Communications.Responses;
+using CashFlow.Communications.Responses.GetAll;
+using CashFlow.Communications.Responses.Register;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.API.Controllers;
@@ -9,6 +13,8 @@ namespace CashFlow.API.Controllers;
 public class ExpensesController : ControllerBase
 {
     [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterExpenseJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Register(
         [FromServices] IRegisterExpenseUseCase useCase,
         [FromBody] RequestRegisterExpenseJson request
@@ -16,5 +22,16 @@ public class ExpensesController : ControllerBase
     {
         var response = await useCase.Execute(request);
         return Created(string.Empty, response);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(ResponseExpensesJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetAll([FromServices] IGetAllExpensesUseCase useCase)
+    {
+        var response = await useCase.Execute();
+        if (response.Expenses.Any())
+            return Ok(response);
+        return NoContent();
     }
 }
