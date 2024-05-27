4d0dd1ae888863a657f81a7e17f1b3736fb9a92e
caiobretas <caiodinizbretas2001@gmail.com>
2024-05-27 10:34:47 -0300

    Add ResponseShortExpensesJson.cs and ResponseExpensesJson.cs

--- src/CashFlow.Communication/Responses/Register/ResponseRegisterExpenseJson.cs
+++ src/CashFlow.Communication/Responses/Register/ResponseRegisterExpenseJson.cs
@@ -2,5 +2,6 @@ namespace CashFlow.Communications.Responses.Register;
 
 public class ResponseRegisterExpenseJson
 {
+    public long Id { get; set; }
     public string Title { get; set; } = string.Empty;
 }
---
+++ src/CashFlow.Communication/Responses/ResponseShortExpensesJson.cs
@@ -0,0 +1,8 @@
+namespace CashFlow.Communications.Responses;
+
+public class ResponseShortExpensesJson
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+}
